a2c171f5628dfc74c964533ea5edd948cceb7ed1
mcalderaf <mcaldera.tec2@gmail.com>
2016-09-02 18:51:49 -0600

    Initial Commit

---
+++ PidePizzaAW WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  PidePizzaAW WatchKit Extension
+//
+//  Created by Marisela Caldera on 02/09/16.
+//  Copyright © 2016 itchii. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
